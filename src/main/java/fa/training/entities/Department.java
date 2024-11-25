6b3392198b45e895bd8bd96a7d484f151bc5f094
Manhduc13 <manhduc130702@gmail.com>
2024-11-25 10:47:39 +0700

    Add lombok and mapping

---
+++ src/main/java/fa/training/entities/Department.java
@@ -0,0 +1,22 @@
+package fa.training.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+@Table(name = "departments", schema = "dbo")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+    @Column(nullable = false, columnDefinition = "nvarchar(50)", unique = true)
+    String name;
+    @Column(columnDefinition = "nvarchar(500)")
+    String description;
+}
